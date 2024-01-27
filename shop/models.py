e7d821e14667331a9b99c048fe3923b62bdda845
nck4s <nn.2nn@mail.ru>
2024-01-27 20:04:24 +1000

    fixed some things

--- shop/models.py
+++ shop/models.py
@@ -1,23 +1,13 @@
 from django.db import models
 
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
 
-
     def __str__(self):
         return self.name
-    
 
-class Order(models.Model):
-    customer_name = models.CharField(max_length=100)
-    products = models.ManyToManyField(Product)
-
-    def __str__(self):
-        return f'Order #{self.pk}'
-    
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
@@ -25,3 +15,11 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f'Order #{self.pk}'
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Product, Order, Customer
-
+from django.db import IntegrityError
 
 def product_list(request):
     products = Product.objects.all()
@@ -8,18 +8,25 @@ def product_list(request):
 
 
 def place_order(request):
-         if request.method == 'POST':
-             customer = Customer.objects.create(
-                 name=request.POST['customer_name'],
-                 email=request.POST['customer_email']
-             )
-             order = Order.objects.create(customer_name=customer.name)
-             order.products.set(request.POST.getlist('product_ids[]'))
-             return redirect('order_success')
-         else:
-             products = Product.objects.all()
-             return render(request, 'place_order.html', {'products': products})
-        
+    if request.method == 'POST':
+        customer_name = request.POST.get('customer_name')
+        customer_email = request.POST.get('customer_email')
 
+        if customer_name and customer_email:
+            customer, created = Customer.objects.get_or_create(
+                name=customer_name,
+                email=customer_email
+            )
 
+            order = Order.objects.create(customer=customer)
+            order.products.set(request.POST.getlist('product_ids[]'))
 
+            return redirect('order_success')
+        else:
+            # If customer information is missing, add an error message
+            error_message = "Customer information is missing."
+            products = Product.objects.all()
+            return render(request, 'place_order.html', {'products': products, 'error_message': error_message})
+    else:
+        products = Product.objects.all()
+        return render(request, 'place_order.html', {'products': products})
